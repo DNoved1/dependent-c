db6df911d7dc0f83780bd5bf947d3ab47f139b42
DNoved1 <dnoved1@aim.com>
2015-09-05 20:11:14 -0400

    Removed unused topological sort code

--- include/dependent-c/type.h
+++ include/dependent-c/type.h
@@ -6,14 +6,6 @@
 #include "dependent-c/ast.h"
 #include "dependent-c/general.h"
 
-/* Analyze the dependencies in the type signatures of top level declarations,
- * returning true and the order if successful. If there is a cyclical
- * dependency in the types a message is printed to stderr and false is
- * returned.
- */
-bool top_level_topological_sort(size_t len, const TopLevel top_levels[len],
-    size_t order[len]);
-
 bool type_check(Context *context, const Expr *expr, const Expr *type);
 bool type_infer(Context *context, const Expr *expr, Expr *result);
 bool type_equal(Context *context, const Expr *type1, const Expr *type2);
