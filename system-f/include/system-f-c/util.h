00890b073bbe1fd566c56787745e3853045043d0
DNoved1 <dnoved1@aim.com>
2015-10-03 13:09:57 -0400

    Indenting util namespace

--- system-f/include/system-f-c/util.h
+++ system-f/include/system-f-c/util.h
@@ -5,36 +5,36 @@
 
 namespace util {
 
-template <typename T>
-void set_union(std::unordered_set<T>& dst, const std::unordered_set<T>& src) {
-    dst.insert(src.begin(), src.end());
-}
-
-template <typename T>
-bool set_contains(const std::unordered_set<T>& set, const T& member) {
-    return set.find(member) != set.end();
-}
-
-template <typename T>
-class Lazy {
-    boost::optional<T> data;
-    std::function<T()> thunk;
-
-  public:
-    Lazy(std::function<T()> thunk) : data(), thunk(thunk) {}
-
-    bool is_forced() const {
-        return (bool)data;
+    template <typename T>
+    void set_union(std::unordered_set<T>& dst, const std::unordered_set<T>& src) {
+        dst.insert(src.begin(), src.end());
     }
 
-    T& force() {
-        if (!data) {
-            data = thunk();
+    template <typename T>
+    bool set_contains(const std::unordered_set<T>& set, const T& member) {
+        return set.find(member) != set.end();
+    }
+
+    template <typename T>
+    class Lazy {
+        boost::optional<T> data;
+        std::function<T()> thunk;
+
+      public:
+        Lazy(std::function<T()> thunk) : data(), thunk(thunk) {}
+
+        bool is_forced() const {
+            return (bool)data;
         }
 
-        return data.get();
-    }
-};
+        T& force() {
+            if (!data) {
+                data = thunk();
+            }
+
+            return data.get();
+        }
+    };
 
 } /* namespace util */
 
