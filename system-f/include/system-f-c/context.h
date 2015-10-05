7fa9b54b5fece4fb0716204de72c0eabc4530ead
DNoved1 <dnoved1@aim.com>
2015-10-04 21:30:59 -0400

    Context now can store term and type definitions

--- system-f/include/system-f-c/context.h
+++ system-f/include/system-f-c/context.h
@@ -8,21 +8,35 @@
 namespace context {
 
     class ContextMarker {
-        size_t term_marker;
-        size_t type_marker;
+        size_t term_type_mark;
+        size_t type_kind_mark;
+        size_t term_definition_mark;
+        size_t type_definition_mark;
 
         friend class Context;
 
-        ContextMarker(size_t term_marker, size_t type_marker)
-            : term_marker(term_marker), type_marker(type_marker) {}
+        ContextMarker(
+            size_t term_type_mark,
+            size_t type_kind_mark,
+            size_t term_definition_mark,
+            size_t type_definition_mark) :
+            term_type_mark(term_type_mark),
+            type_kind_mark(type_kind_mark),
+            term_definition_mark(term_definition_mark),
+            type_definition_mark(type_definition_mark)
+            {}
     };
 
     class Context {
         std::vector<std::pair<std::string, ast::expr::Expr>> term_context;
         std::vector<std::pair<std::string, ast::expr::Expr>> type_context;
 
+        std::vector<std::pair<std::string, ast::expr::Expr>> term_defines;
+        std::vector<std::pair<std::string, ast::expr::Expr>> type_defines;
+
       public:
-        Context() : term_context(), type_context() {}
+        Context() : term_context(), type_context(),
+            term_defines(), type_defines() {}
 
         ContextMarker mark();
         void reset(ContextMarker marker);
@@ -32,6 +46,12 @@ namespace context {
 
         void register_type(std::string name, ast::expr::Expr kind);
         boost::optional<ast::expr::Expr> lookup_type(std::string name);
+
+        void define_term(std::string name, ast::expr::Expr term);
+        boost::optional<ast::expr::Expr> lookup_term_definition(std::string name);
+
+        void define_type(std::string name, ast::expr::Expr type);
+        boost::optional<ast::expr::Expr> lookup_type_definition(std::string name);
     };
 }
 
--- system-f/src/context.cpp
+++ system-f/src/context.cpp
@@ -14,17 +14,25 @@ using util::removing_resize;
 namespace context {
 
 ContextMarker Context::mark() {
-    return ContextMarker(this->term_context.size(), this->type_context.size());
+    return ContextMarker(
+        this->term_context.size(),
+        this->type_context.size(),
+        this->term_defines.size(),
+        this->type_defines.size());
 }
 
-void Context::reset(ContextMarker marker) {
-    if (marker.term_marker > this->term_context.size()
-            || marker.type_marker > this->type_context.size()) {
+void Context::reset(ContextMarker mark) {
+    if (mark.term_type_mark > this->term_context.size()
+            || mark.type_kind_mark > this->type_context.size()
+            || mark.term_definition_mark > this->term_defines.size()
+            || mark.type_definition_mark > this->type_defines.size()) {
         throw "Invalid marker";
     }
 
-    removing_resize(this->term_context, marker.term_marker);
-    removing_resize(this->type_context, marker.type_marker);
+    removing_resize(this->term_context, mark.term_type_mark);
+    removing_resize(this->type_context, mark.type_kind_mark);
+    removing_resize(this->term_defines, mark.term_definition_mark);
+    removing_resize(this->type_defines, mark.type_definition_mark);
 }
 
 void Context::register_term(string name, Expr type) {
@@ -57,5 +65,34 @@ optional<Expr> Context::lookup_type(string name) {
     return optional<Expr>();
 }
 
+void Context::define_term(string name, Expr term) {
+    this->term_defines.push_back(make_pair(name, term));
+}
+
+optional<Expr> Context::lookup_term_definition(string name) {
+    for (auto iter = this->term_defines.rbegin();
+            iter != this->term_defines.rend(); iter++) {
+        if (iter->first == name) {
+            return make_optional(iter->second);
+        }
+    }
+
+    return optional<Expr>();
+}
+
+void Context::define_type(string name, Expr type) {
+    this->type_defines.push_back(make_pair(name, type));
+}
+
+optional<Expr> Context::lookup_type_definition(string name) {
+    for (auto iter = this->type_defines.rbegin();
+            iter != this->type_defines.rend(); iter++) {
+        if (iter->first == name) {
+            return make_optional(iter->second);
+        }
+    }
+
+    return optional<Expr>();
+}
 
 } /* namespace context */
